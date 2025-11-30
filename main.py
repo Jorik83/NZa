f2c2b885b7b406036d436b9e463c182caa7d41fb
JorikdeGraaf <Jorikdegraaf@hotmail.com>
2025-11-30 20:01:46 +0100

    1e werkende versie

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 # Auteur: Jorik de Graaf
 
+import zeep
+
+# WSDL URL
+wsdl_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL"
+
+# Start Zeep client
+client = zeep.Client(wsdl=wsdl_url)
+
+# Lijst met landen
+countries = client.service.ListOfCountryNamesByCode()
+
+# Vervang '&' met 'and' en 'And' met 'and' voor consistentie 
+for row in countries:
+    row.sName = row.sName.replace('&','and').replace('And','and')
+    print(row.sName, row.sISOCode)
